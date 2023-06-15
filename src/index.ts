839e514f564206de921c0d1d8c0d7ae901c1c955
lafkpages <mail@lafkpages.tech>
2023-06-15 20:42:38 +0200

    Comment

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { applyGlobalThemeValuesToElement } from "./util/theme";
 
 // Get theme
 chrome.storage.local.get(['themeValues'], ({ themeValues }) => {
+  // Apply theme
   applyGlobalThemeValuesToElement(themeValues, document.body);
 });
