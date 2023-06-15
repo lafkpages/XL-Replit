7d18549353e5ff7d6c0f0565a1f04996a33d61e3
lafkpages <mail@lafkpages.tech>
2023-06-15 18:25:56 +0200

    getTheme util file

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-import { ReplitAccent, replitAccents, ReplitAccentVariations, replitAccentVariations, replitAccentVariationsBasic, ReplitAccentVariationsBasic, ReplitThemeGlobalValues } from "./types";
+import { getThemeGlobalValues } from "./util/getTheme";
 
 document.addEventListener('DOMContentLoaded', (e) => {
   console.debug('[XL] Reading SID from CRX storage');
@@ -43,43 +43,7 @@ window.addEventListener('load', (e) => {
   }
 
   // Save theme values
-  // TODO: make this an util func
-  const themeContainerStyles = getComputedStyle(
-    document.querySelector('.replit-ui-theme-root') ||
-    document.documentElement
-  );
-
-  const themeValues: ReplitThemeGlobalValues = {
-    accents: {},
-    backgrounds: {},
-  };
-
-  for (const accent of replitAccents) {
-    for (const variation of replitAccentVariationsBasic) {
-      const value = themeContainerStyles.getPropertyValue(
-        `--accent-${accent}-${variation}`
-      );
-
-      if (value) {
-        if (!themeValues.accents[accent]) {
-          themeValues.accents[accent] = {};
-        }
-
-        themeValues.accents[accent]![variation] = value;
-      }
-    }
-  }
-
-  for (const variation of replitAccentVariations) {
-    const value = themeContainerStyles.getPropertyValue(
-      `--background-${variation}`
-    );
-
-    if (value) {
-      themeValues.backgrounds[variation] = value;
-    }
-  }
-
+  const themeValues = getThemeGlobalValues();
   console.log('[XL] Got theme values:', themeValues);
 });
 
---
+++ src/util/getTheme.ts
@@ -0,0 +1,42 @@
+import { ReplitThemeGlobalValues, replitAccentVariations, replitAccentVariationsBasic, replitAccents } from "../types";
+
+export function getThemeGlobalValues(elm?: HTMLElement) {
+  const themeContainerStyles = getComputedStyle(
+    elm ||
+    document.querySelector('.replit-ui-theme-root') ||
+    document.documentElement
+  );
+
+  const themeValues: ReplitThemeGlobalValues = {
+    accents: {},
+    backgrounds: {},
+  };
+
+  for (const accent of replitAccents) {
+    for (const variation of replitAccentVariationsBasic) {
+      const value = themeContainerStyles.getPropertyValue(
+        `--accent-${accent}-${variation}`
+      );
+
+      if (value) {
+        if (!themeValues.accents[accent]) {
+          themeValues.accents[accent] = {};
+        }
+
+        themeValues.accents[accent]![variation] = value;
+      }
+    }
+  }
+
+  for (const variation of replitAccentVariations) {
+    const value = themeContainerStyles.getPropertyValue(
+      `--background-${variation}`
+    );
+
+    if (value) {
+      themeValues.backgrounds[variation] = value;
+    }
+  }
+
+  return themeValues;
+}
