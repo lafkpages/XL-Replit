b0f9942915c6cf8d60864c938f796df03fa5e3e4
lafkpages <mail@lafkpages.tech>
2023-06-16 21:02:20 +0200

    Build script spinners!

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -10,7 +10,11 @@ const distDir = 'dist';
 const nodeModulesDir = path.resolve('./node_modules');
 
 // Build types for Node, to use here
-await $`./node_modules/.bin/esbuild ./src/types --outfile=${distDir}/types.js --bundle --minify --target=node12 --format=cjs`;
+await spinner(
+  'Building types',
+  () =>
+    $`./node_modules/.bin/esbuild ./src/types --outfile=${distDir}/types.js --bundle --minify --target=node12 --format=cjs`
+);
 const { replitAccents } = require(`../${distDir}/types.js`);
 await fs.rm(`${distDir}/types.js`);
 
@@ -57,67 +61,81 @@ if (argv.dev || argv._[0] == 'dev') {
 const buildDir = `${distDir}/${browser}`;
 
 // Remove old builds
-if (await fs.pathExists(`${distDir}/manifest.json`)) {
-  await fs.emptyDir(distDir);
-} else {
-  await fs.emptyDir(buildDir);
-}
+await spinner('Cleaning up', async () => {
+  if (await fs.pathExists(`${distDir}/manifest.json`)) {
+    await fs.emptyDir(distDir);
+  } else {
+    await fs.emptyDir(buildDir);
+  }
 
-await fs.ensureDir(buildDir);
+  await fs.ensureDir(buildDir);
+});
 
 // Copy manifest
-const manifest = await fs.readJson('src/manifest.json');
-delete manifest['$schema'];
-switch (browser) {
-  case 'firefox': {
-    manifest.background.scripts = [manifest.background.service_worker];
-    delete manifest.background.service_worker;
-    break;
-  }
+let manifest = null;
+await spinner('Building manifest', async () => {
+  manifest = await fs.readJson('src/manifest.json');
+  delete manifest['$schema'];
+  switch (browser) {
+    case 'firefox': {
+      manifest.background.scripts = [manifest.background.service_worker];
+      delete manifest.background.service_worker;
+      break;
+    }
 
-  case 'chrome': {
-    delete manifest.developer;
+    case 'chrome': {
+      delete manifest.developer;
+    }
   }
-}
-await fs.writeJson(`${buildDir}/manifest.json`, manifest);
+  await fs.writeJson(`${buildDir}/manifest.json`, manifest);
+});
 
 // Copy localization files
-await fs.copy(`src/locales`, `${buildDir}/_locales`);
+await spinner('Copying localization files', () =>
+  fs.copy(`src/locales`, `${buildDir}/_locales`)
+);
 
 // Copy public files
-await fs.copy('public', `${buildDir}/public`);
+await spinner('Copying public files', () =>
+  fs.copy('public', `${buildDir}/public`)
+);
 
 // Copy HTML, CSS and net rules
 for (const folder of ['html', 'css', 'net-rules']) {
-  await fs.copy(`src/${folder}`, `${buildDir}/${folder}`);
+  await spinner(`Copying ${folder}`, () =>
+    fs.copy(`src/${folder}`, `${buildDir}/${folder}`)
+  );
 }
 
 // Inject variant classes into XL CSS
-let xlCss = await fs.readFile(`${buildDir}/css/xl.css`, 'utf8');
-for (const accent of replitAccents) {
-  xlCss += `
-    .${accent}-fg {
-      color: var(--accent-${accent}-default);
-    }
+await spinner('Building XL CSS', async () => {
+  let xlCss = await fs.readFile(`${buildDir}/css/xl.css`, 'utf8');
+  for (const accent of replitAccents) {
+    xlCss += `
+      .${accent}-fg {
+        color: var(--accent-${accent}-default);
+      }
 
-    .${accent}-bg {
-      background-color: var(--accent-${accent}-default);
-    }
-  `.replace(/\s+/g, '');
-}
-await fs.writeFile(`${buildDir}/css/xl.css`, xlCss);
+      .${accent}-bg {
+        background-color: var(--accent-${accent}-default);
+      }
+    `.replace(/\s+/g, '');
+  }
+  await fs.writeFile(`${buildDir}/css/xl.css`, xlCss);
+});
 
 // Copy Monaco editor from Node modules
 const monacoMode = isDev ? 'dev' : 'min';
-await fs.copy(
-  `node_modules/monaco-editor/${monacoMode}/vs`,
-  `${buildDir}/public/vs`
+await spinner('Copying Monaco', () =>
+  fs.copy(
+    `node_modules/monaco-editor/${monacoMode}/vs`,
+    `${buildDir}/public/vs`
+  )
 );
 
 // Copy RequireJS lib
-await fs.copy(
-  'node_modules/requirejs/require.js',
-  `${buildDir}/public/require.js`
+await spinner('Copying RequireJS', () =>
+  fs.copy('node_modules/requirejs/require.js', `${buildDir}/public/require.js`)
 );
 
 // ESBuild options
@@ -131,21 +149,30 @@ if (isDev) {
 }
 
 // Build TypeScript files into JavaScript
-await $`./node_modules/.bin/esbuild src/inject.ts ${[
-  `--outdir=${buildDir}`,
-  ...opts,
-  '--global-name=xlReplit',
-]}`;
-await $`./node_modules/.bin/esbuild src/{background,popup,content,index}.ts src/util/ot.ts --outdir=${buildDir} ${opts}`;
+await spinner('Building TypeScript', async () => {
+  await $`./node_modules/.bin/esbuild src/inject.ts ${[
+    `--outdir=${buildDir}`,
+    ...opts,
+    '--global-name=xlReplit',
+  ]}`;
+  await $`./node_modules/.bin/esbuild src/{background,popup,content,index}.ts src/util/ot.ts --outdir=${buildDir} ${opts}`;
+});
 
 // If prod, bundle
 if (!isDev) {
-  await $`cd ${buildDir} && ${nodeModulesDir}/.bin/web-ext build -o`;
+  await spinner('Bundling', async () => {
+    await $`cd ${buildDir} && ${nodeModulesDir}/.bin/web-ext build -o`;
 
-  await fs.move(
-    `${buildDir}/web-ext-artifacts/xl_replit-${manifest.version}.zip`,
-    `${buildDir}/../${browser}.zip`
-  );
+    const bundleFile = `${buildDir}/web-ext-artifacts/xl_replit-${manifest.version}.zip`;
+    const bundleDest = `${buildDir}/../${browser}.zip`;
 
-  await fs.remove(`${buildDir}/web-ext-artifacts`);
+    try {
+      await fs.remove(bundleDest);
+      await fs.move(bundleFile, bundleDest);
+    } catch {
+      echo(`Failed to move bundle`);
+    }
+
+    await fs.remove(`${buildDir}/web-ext-artifacts`);
+  });
 }
