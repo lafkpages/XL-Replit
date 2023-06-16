21eed6afd93254cde1cf39696cbc0a15b38db660
lafkpages <mail@lafkpages.tech>
2023-06-16 10:22:30 +0200

    Move theme types to theme util

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { api as replitProtocol } from '@replit/protocol';
+import type { ReplitCustomThemeValues } from './util/theme';
 
 export interface OTv1 {
   insert?: string;
@@ -92,54 +93,9 @@ export interface ReplitReduxState {
   };
 }
 
-export interface ReplitCustomThemeValues {
-  editor: {
-    syntaxHighlighting: ReplitThemeEditorValue[];
-  };
-  global: {
-    [key: string]: string;
-  };
-}
-
 export interface GraphQLResponse {
   __typename?: string;
   [key: string]: any;
-}
-
-export interface ReplitThemeEditorValue {
-  __typename?: 'ThemeEditorSyntaxHighlighting';
-  values?: {
-    [key: string]: string;
-  };
-  tags?: (GraphQLResponse & {
-    __typename?: 'ThemeEditorTag';
-    modifiers?: string[] | null;
-    name?: string;
-  })[];
-}
-
-export type ReplitThemeGlobalValuesProp = {
-  [key in ReplitAccentVariation]?: string;
-};
-
-export type ReplitThemeGlobalValuesAccentsProp = {
-  [key in ReplitAccent]?: {
-    [key in ReplitAccentVariationBasic]?: string;
-  };
-};
-
-export const replitThemeGlobalValuesProps = [
-  'background',
-  'foreground',
-  'outline',
-] as const;
-
-export type ReplitThemeGlobalValuesProps = (typeof replitThemeGlobalValuesProps)[number];
-
-export type ReplitThemeGlobalValues = {
-  accents: ReplitThemeGlobalValuesAccentsProp;
-} & {
-  [key in ReplitThemeGlobalValuesProps]?: ReplitThemeGlobalValuesProp;
 }
 
 export interface ReplitEditorPreferences {
@@ -157,48 +113,6 @@ export interface XLGovalChannel {
   handler?: (data: any) => void;
   openChanRes?: replitProtocol.Command;
 }
-
-export const replitAccents = [
-  'primary',
-  'positive',
-  'negative',
-  'red',
-  'orange',
-  'yellow',
-  'lime',
-  'green',
-  'teal',
-  'blue',
-  'blurple',
-  'purple',
-  'magenta',
-  'pink',
-  'grey',
-  'brown',
-] as const;
-
-export type ReplitAccent = (typeof replitAccents)[number];
-
-export const replitAccentVariationsBasic = [
-  'dimmest',
-  'dimmer',
-  'default',
-  'stronger',
-  'strongest',
-] as const;
-
-export type ReplitAccentVariationBasic =
-  (typeof replitAccentVariationsBasic)[number];
-
-export const replitAccentVariations = [
-  'root',
-  ...replitAccentVariationsBasic,
-  'overlay',
-  'higher',
-  'highest',
-] as const;
-
-export type ReplitAccentVariation = (typeof replitAccentVariations)[number];
 
 // TODO: import from crypto
 export type UUID = `${string}-${string}-${string}-${string}-${string}`;
